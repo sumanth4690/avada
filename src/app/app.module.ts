6bdd73c00e79838223b238ef41c046ccd3566653
sumanth4690 <sumanthgoud469@gmail.com>
2022-03-14 14:04:53 +0530

    added navbar menu

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,15 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { NavbarComponent } from './components/navbar/navbar.component';
+import { MenuComponent } from './components/menu/menu.component';
+
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavbarComponent,
+    MenuComponent
   ],
   imports: [
     BrowserModule,
